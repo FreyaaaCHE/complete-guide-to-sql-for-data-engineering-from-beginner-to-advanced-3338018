9f9a25a831dbfaf26445bf9aa419e1de980c375b
FreyaaaCHE <152638141+FreyaaaCHE@users.noreply.github.com>
2025-09-26 19:28:59 +0000

    Update SQL code and rename files

--- 06-07 Moving average.sql
+++ 06-07 Moving average.sql
@@ -2,4 +2,11 @@ select order_id, customer_id, order_timestamp, total_amount,
 
 avg(total_amount) over (partition by customer_id order by order_timestamp
 	rows between 1 preceding and current row) as mvg_avg
-	from orders
+	from orders;
+
+
+select product_id, order_timestamp, total_quantity,
+
+sum(total_quantity) over(partition by product_id order by order_timestamp) as running_qty
+
+from orders;
